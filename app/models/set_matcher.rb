960c804025d44dc7a19c800ddc961e3c6b930a8d
Clint Thacker <couscous@gmail.com>
2017-09-20 17:12:42 -0500

    Add SetMatcher helper class

---
+++ app/models/set_matcher.rb
@@ -0,0 +1,42 @@
+class SetMatcher
+
+
+  def self.colors_for_set(cards)
+    card_1 = cards[0]
+    card_2 = cards[1]
+    card_3 = cards[2]
+
+    (card_1.color == card_2.color && card_1.color == card_3.color && card_2.color == card_3.color) || (card_1.color != card_2.color && card_1.color != card_3.color && card_2.color != card_3.color)
+  end
+
+  def self.shapes_for_set(cards)
+    card_1 = cards[0]
+    card_2 = cards[1]
+    card_3 = cards[2]
+
+    (card_1.shape == card_2.shape && card_1.shape == card_3.shape && card_2.shape == card_3.shape) || (card_1.shape != card_2.shape && card_1.shape != card_3.shape && card_2.shape != card_3.shape)
+  end
+
+  def self.shadings_for_set(cards)
+    card_1 = cards[0]
+    card_2 = cards[1]
+    card_3 = cards[2]
+
+    (card_1.shading == card_2.shading && card_1.shading == card_3.shading && card_2.shading == card_3.shading) || (card_1.shading != card_2.shading && card_1.shading != card_3.shading && card_2.shading != card_3.shading)
+  end
+
+  def self.numbers_for_set(cards)
+    card_1 = cards[0]
+    card_2 = cards[1]
+    card_3 = cards[2]
+
+    (card_1.number == card_2.number && card_1.number == card_3.number && card_2.number == card_3.number) || (card_1.number != card_2.number && card_1.number != card_3.number && card_2.number != card_3.number)
+  end
+
+
+  # does this logic belong here?
+  def self.is_a_set?(cards)
+    colors_for_set(cards) && shadings_for_set(cards) && numbers_for_set(cards) && shapes_for_set(cards)
+  end
+
+end
