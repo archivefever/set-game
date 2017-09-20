ff71b4e208f7f8ab7ff7d03158e2c5fe7367bf53
Dan Ioppolo <email": null>
2017-09-20 12:10:53 -0500

    login and register

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,36 @@
+class PlayersController < ApplicationController
+
+  def new
+    @user = Player.new
+  end
+
+  def show
+    set_player
+    if current_user && current_user.id == @player.id
+      render :show
+    else
+      redirect_to '/'
+    end
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      session[:player_id] = @player.id
+      redirect_to "/players/#{@player.id}"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_player
+    @player = Player.find_by(id: params[:id])
+  end
+
+  def player_params
+    params.require(:player).permit(:username, :password, :password_confirmation)
+  end
+
+end
--- db/migrate/20170920160353_create_players.rb
+++ db/migrate/20170920160353_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[5.1]
   def change
     create_table :players do |t|
       t.string    :username, null:false
-      t.string     :email, null: false
+      t.string    :email, null: false
       t.string    :password_digest
 
       t.timestamps
