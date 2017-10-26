83ccb9fc2a3467fd67c3732e09a30a42ef9e30b3
Marla Kallstrom <brandykou@gmail.com>
2017-10-25 19:38:04 -0500

    fix model tests so they account for next_deal running twice when there are no valid sets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920203353) do
+ActiveRecord::Schema.define(version: 20171025202642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20170920203353) do
     t.string "username", null: false
     t.string "email", null: false
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "seeks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -26,16 +26,16 @@ describe Game do
   end
 
   describe "game after first deal" do
-    it "has 9 showing cards" do
+    it "has at least 9 showing cards" do
       game.load_deck
       game.initial_deal
-      expect(game.showing_cards.count).to eq 9
+      expect(game.showing_cards.count).to be >= 9
     end
 
-    it "has 72 undrawn cards" do
+    it "has a max of 72 undrawn cards" do
       game.load_deck
       game.initial_deal
-      expect(game.undrawn_cards.count).to eq 72
+      expect(game.undrawn_cards.count).to be <= 72
     end
 
     it "has 0 grouped cards" do
@@ -46,21 +46,21 @@ describe Game do
   end
 
   describe "game after second deal" do
-    it "has 9 showing cards" do
+    it "deals three or more cards until there is a valid set on the board" do
       game.load_deck
       game.initial_deal
       game.next_deal
-      expect(game.showing_cards.count).to eq 12
+      expect(game.showing_cards.count).to be >= 12
     end
 
-    it "has 72 undrawn cards" do
+    it "updates undrawn cards when dealing additional ones" do
       game.load_deck
       game.initial_deal
       game.next_deal
-      expect(game.undrawn_cards.count).to eq 69
+      expect(game.undrawn_cards.count).to be <= 69
     end
 
-    it "has 0 grouped cards" do
+    it "has 0 grouped cards when user hasn't made any sets" do
       game.load_deck
       game.initial_deal
       game.next_deal
