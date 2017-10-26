25631e05e98f27d06e3d07a1214500b10235a158
Dan Ioppolo <email": null>
2017-10-26 13:26:38 -0500

    add finsihed attribute to game model

--- db/migrate/20170920160429_create_games.rb
+++ db/migrate/20170920160429_create_games.rb
@@ -2,6 +2,7 @@ class CreateGames < ActiveRecord::Migration[5.1]
   def change
     create_table :games do |t|
       t.integer   :player_id
+      t.boolean   :finished, { default: false }
 
       t.timestamps
     end
