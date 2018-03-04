98fd64de80f6461e98ceac990c90cf0ec8abe858
Clint Thacker <couscous@gmail.com>
2018-03-04 14:21:43 -0600

    Add game serializer, per tutorial; not yet certain if this is appropriate or useful for our purposes

---
+++ app/serializers/game_serializer.rb
@@ -0,0 +1,11 @@
+class GameSerializer < ActiveModel::Serializer
+  attributes :id, :finished, :winner, :board, :created_at, :updated_at
+  has_and_belongs_to_many :players, optional: true
+  has_many :game_cards
+  has_many :cards, through: :game_cards
+
+  has_many :undrawn_cards, ->{ where("game_cards.status" => "undrawn")}, through: :game_cards, source: :card
+  has_many :showing_cards, ->{ where("game_cards.status" => "showing")}, through: :game_cards, source: :card
+  has_many :grouped_cards, ->{ where("game_cards.status" => "grouped")}, through: :game_cards, source: :card
+
+end
